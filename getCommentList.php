9657502ec0c75d0ab88195034ec5caf6dbf002fc
shna231 <71744479+shna231@users.noreply.github.com>
2020-12-01 01:44:59 +0900

    Create getCommentList.php

---
+++ getCommentList.php
@@ -0,0 +1,35 @@
+<?php  
+error_reporting(E_ALL); 
+ini_set('display_errors',1); 
+
+include('dbcon.php');
+
+//POST 값을 읽어온다.
+$article_num=isset($_POST['article_num']) ? $_POST['article_num'] : '';
+
+//사용자의 댓글 리스트 읽어오기. where writer_id='$writer_id'
+$sql="select * from booktu.comment where article_num='$article_num'";
+
+$stmt=$con->prepare($sql);
+$stmt->execute();
+ 
+if ($stmt->rowCount() == 0){
+    echo "댓글이 존재하지 않습니다";
+	
+} else{
+
+   	$data=array(); 
+	// extract($row);
+
+	while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
+        	array_push($data,array('comment_num'=>$row["comment_num"],
+        			                   'article_num'=>$row["article_num"],
+				                         'writer_id'=>$row["writer_id"],
+                                 'content'=>$row["content"]));
+	}
+	
+	header('Content-Type: application/json; charset=utf8');
+	echo json_encode(array("data"=>$data), JSON_PRETTY_PRINT+JSON_UNESCAPED_UNICODE);
+    } 
+
+?>

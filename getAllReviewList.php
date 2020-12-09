5875eefbf86fccd3225111df790b72dd072097db
shna231 <71744479+shna231@users.noreply.github.com>
2020-12-10 01:45:23 +0900

    Update getAllReviewList.php

--- getAllReviewList.php
+++ getAllReviewList.php
@@ -20,6 +20,14 @@ if(!$rBookName=="" || !$rBookWriter=="" || !$rReviewName=="" || !$rReviewWriter=
     $sql=$sql.$rBookName.'$search_word';
   }
   
+  // 두번째 옵션이 체크
+  if(!$rBookWriter==""){
+    // 앞 옵션 중 하나라도 있었다면 AND 추가 후 붙임
+    if(!$rBookName==""){
+      $sql=$sql." AND";
+    }
+    $sql=$sql.$rBookWriter.'$search_word';
+  }
 
 
 echo $sql;

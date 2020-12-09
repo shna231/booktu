0b944327c3f4021e233c3f5031e741dd63ebd139
shna231 <71744479+shna231@users.noreply.github.com>
2020-12-09 11:51:24 +0900

    Update getCommentList.php

--- getCommentList.php
+++ getCommentList.php
@@ -6,4 +6,16 @@ include('dbcon.php');
 
 echo "yaho";
 
+//POST 값을 읽어온다.
+$article_num=isset($_POST['article_num']) ? $_POST['article_num'] : '';
+
+//사용자의 댓글 리스트 읽어오기. where writer_id='$writer_id'
+$sql="select * from booktu.comment where article_num='$article_num'";
+
+$stmt=$con->prepare($sql);
+$stmt->execute();
+
+$data=array(); 
+// extract($row);
+
 ?>

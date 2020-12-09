640499fccaee16e6420cbf927dde3956421f6ce9
shna231 <71744479+shna231@users.noreply.github.com>
2020-12-10 00:28:22 +0900

    Update getAllReviewList.php

--- getAllReviewList.php
+++ getAllReviewList.php
@@ -12,52 +12,6 @@ $search_word=isset($_POST['search_word']) ? $_POST['search_word'] : '';
 
 $sql="SELECT * from booktu.report";
 
-if($rBookName | $rBookWriter | $rReviewName | $rReviewWriter) {
-	$sql=$sql." WHERE";
-}
-
-if($rBookName) {
-	$sql=$sql." book_title='$search_word'";
-}
-if($rBookWriter) {
-	if($rBookName){
-		$sql=$sql." AND";
-	}
-	$sql=$sql." book_author='$search_word'";
-}
-if($rReviewName) {
-	if($rBookName | $rBookWriter){
-		$sql=$sql." AND";
-	}
-	$sql= $sql." title='$search_word'";
-}
-if($rReviewWriter) {
-	if($rBookName | $rBookWriter | $rReviewName) {
-		$sql=$sql." AND"
-	}
-	$sql= $sql." writer_id='$search_word'";
-}
-
-
-$stmt=$con->prepare($sql);
-$stmt->execute();
- 
-if ($stmt->rowCount() == 0){
-    echo "독후감이 존재하지 않습니다";
-	
-} else{
-
-   	$data=array(); 
-	// extract($row);
-
-	while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
-        	array_push($data,array('book_image'=>$row["book_image"],
-        			'title'=>$row["title"],
-				'content'=>$row["content"]));
-	}
-	
-	header('Content-Type: application/json; charset=utf8');
-	echo json_encode(array("data"=>$data), JSON_PRETTY_PRINT+JSON_UNESCAPED_UNICODE);
-    } 
+echo "yaho";
 
 ?>

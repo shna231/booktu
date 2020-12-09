25500238309f5eb419826ca78e172eb41e16c7c0
shna231 <71744479+shna231@users.noreply.github.com>
2020-12-10 00:54:31 +0900

    Update getAllReviewList.php

--- getAllReviewList.php
+++ getAllReviewList.php
@@ -12,7 +12,13 @@ $search_word=isset($_POST['search_word']) ? $_POST['search_word'] : '';
 
 $sql="SELECT * from booktu.report";
 
+echo $rBookName
 var_dump($rBookName);
 
+if(!$rBookName=="" || !$rBookWriter=="" || !$rReviewName=="" || !$rReviewWriter=="") {
+	$sql=$sql." WHERE";
+}
+
+echo $sql
 
 ?>

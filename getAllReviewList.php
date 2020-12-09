e4b206acd4ecff36f57c3a6fb71aec03edb5889d
shna231 <71744479+shna231@users.noreply.github.com>
2020-12-10 02:04:34 +0900

    Update getAllReviewList.php

--- getAllReviewList.php
+++ getAllReviewList.php
@@ -15,34 +15,34 @@ if(!$rBookName=="" || !$rBookWriter=="" || !$rReviewName=="" || !$rReviewWriter=
   $sql=$sql." WHERE";
 }
  
-// 첫 옵션이 체크 > AND 없이 붙임
+// 첫 옵션이 체크 > OR 없이 붙임
   if(!$rBookName==""){
     $sql=$sql.$rBookName."'$search_word'";
   }
   
   // 두번째 옵션이 체크
   if(!$rBookWriter==""){
-    // 앞 옵션 중 하나라도 있었다면 AND 추가 후 붙임
+    // 앞 옵션 중 하나라도 있었다면 OR 추가 후 붙임
     if(!$rBookName==""){
-      $sql=$sql." AND";
+      $sql=$sql." OR";
     }
     $sql=$sql.$rBookWriter."'$search_word'";
   }
 
    // 세번째 옵션이 체크
   if(!$rReviewName==""){
-    // 앞 옵션 중 하나라도 있었다면 AND 추가 후 붙임
+    // 앞 옵션 중 하나라도 있었다면 OR 추가 후 붙임
     if(!$rBookName=="" || !$rBookWriter==""){
-      $sql=$sql." AND";
+      $sql=$sql." OR";
     }
     $sql=$sql.$rReviewName."'$search_word'";
   }
   
    // 네번째 옵션이 체크
   if(!$rReviewWriter==""){
-    // 앞 옵션 중 하나라도 있었다면 AND 추가 후 붙임
+    // 앞 옵션 중 하나라도 있었다면 OR 추가 후 붙임
     if(!$rBookName=="" || !$rBookWriter=="" || !$rReviewName==""){
-      $sql=$sql." AND";
+      $sql=$sql." OR";
     }
     $sql=$sql.$rReviewWriter."'$search_word'";
   }

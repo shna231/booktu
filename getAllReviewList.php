cd14a464f8d974a238e568b2a0bc90cc9a3a453d
shna231 <71744479+shna231@users.noreply.github.com>
2020-12-10 01:32:10 +0900

    Update getAllReviewList.php

--- getAllReviewList.php
+++ getAllReviewList.php
@@ -12,7 +12,40 @@ $search_word=isset($_POST['search_word']) ? $_POST['search_word'] : '';
 
 $sql="SELECT * from booktu.report";
 if(!$rBookName=="" || !$rBookWriter=="" || !$rReviewName=="" || !$rReviewWriter=="") {
-  $sql=$sql.$rBookName.$rBookWriter.$rReviewName.$rReviewWriter;
+  $sql=$sql." WHERE";
+  
+  // 첫 옵션이 체크 > AND 없이 붙임
+  if(!$rBookName==""){
+    $sql=$sql.$rBookName.'$search_word';
+  }
+  
+  // 두번째 옵션이 체크
+  if(!$rBookWriter==""){
+    // 앞 옵션 중 하나라도 있었다면 AND 추가 후 붙임
+    if(!$rBookName==""){
+      $sql=$sql." AND"
+    }
+    $sql=$sql.$rBookWriter.'$search_word';
+  }
+  
+   // 세번째 옵션이 체크
+  if(!$rReviewName==""){
+    // 앞 옵션 중 하나라도 있었다면 AND 추가 후 붙임
+    if(!$rBookName=="" || !$rBookWriter==""){
+      $sql=$sql." AND"
+    }
+    $sql=$sql.$rBookWriter.'$search_word';
+  }
+  
+   // 네번째 옵션이 체크
+  if(!$rReviewName==""){
+    // 앞 옵션 중 하나라도 있었다면 AND 추가 후 붙임
+    if(!$rBookName=="" || !$rBookWriter=="" || !$rReviewName==""){
+      $sql=$sql." AND"
+    }
+    $sql=$sql.$rBookWriter.'$search_word';
+  }
+  
 }
  
 echo $sql;

e3559db660184bbff2b986a2935e2e578be4a0bd
shna231 <71744479+shna231@users.noreply.github.com>
2020-12-11 04:37:46 +0900

    Update addTopiLike.php

--- addTopiLike.php
+++ addTopiLike.php
@@ -9,7 +9,6 @@ $topic_num=isset($_POST['topic_num']) ? $_POST['topic_num'] : '';
 $android=strpos($_SERVER['HTTP_USER_AGENT'], "Android");
 
 $sql1="SELECT liked FROM booktu.discussion_topic WHERE topic_num='$topic_num'";
-$sql1 += 1;
 
 echo $sql1;
 

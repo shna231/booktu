c4c0ab5f87d0cef3859dd58309eb261cd3b7621d
shna231 <71744479+shna231@users.noreply.github.com>
2020-12-11 04:35:09 +0900

    Create addTopiLike.php

---
+++ addTopiLike.php
@@ -0,0 +1,17 @@
+<?php 
+error_reporting(E_ALL); 
+ini_set('display_errors',1); 
+
+include('dbcon.php');
+
+$topic_num=isset($_POST['topic_num']) ? $_POST['topic_num'] : '';
+
+$android=strpos($_SERVER['HTTP_USER_AGENT'], "Android");
+
+$sql1="SELECT liked FROM booktu.discussion_topic WHERE topic_num='$topic_num'";
+$sql1 += 1;
+
+val_dump($sql1);
+
+
+?>

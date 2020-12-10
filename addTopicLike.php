0e74741765c4138631bb0b870f92e5eafb06f7c7
shna231 <71744479+shna231@users.noreply.github.com>
2020-12-11 04:41:48 +0900

    Update and rename addTopiLike.php to addTopicLike.php

--- addTopicLike.php
+++ addTopicLike.php
@@ -5,12 +5,15 @@ ini_set('display_errors',1);
 include('dbcon.php');
 
 $topic_num=isset($_POST['topic_num']) ? $_POST['topic_num'] : '';
+
+$android=strpos($_SERVER['HTTP_USER_AGENT'], "Android");
+
 $sql1="SELECT liked FROM booktu.discussion_topic WHERE topic_num='$topic_num'";
 
 $stmt=$con->prepare($sql1);
 $stmt->execute();
 
-echo $sql1;
+echo $stmt;
 
 
 ?>
